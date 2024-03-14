2aa9207d419d8f627e930931b19924f8165b121d
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-03-14 09:48:46 +0100

    feat: Add subshell test cases

---
+++ cmds/bonus/3_subshell.sh
@@ -0,0 +1,10 @@
+# **************************************************************************** #
+#                                  SUBSHELL                                    #
+# **************************************************************************** #
+yes | (head -1 && (head -2 | (head -3 && head -4))) | cat | wc -l
+
+yes | (echo 1 && (echo 2 | (echo 3 && echo 4))) | cat
+
+yes | (echo 1 && (echo 2 | (echo 3 && echo 4))) | cat | wc -l
+
+yes | (head -1 && (head -2 | (head -3 && head -4))) | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | wc -l

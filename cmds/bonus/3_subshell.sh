944f3ba7de221fe865b14b51bd14538a378d6ef7
LeaYeh <smile2140@gmail.com>
2024-03-15 14:47:19 +0100

    feat: Add all test cases about redir of subshell

--- cmds/bonus/3_subshell.sh
+++ cmds/bonus/3_subshell.sh
@@ -10,3 +10,21 @@ yes | (echo 1 && (echo 2 | (echo 3 && echo 4))) | cat | wc -l
 yes | (head -1 && (head -2 | (head -3 && head -4))) | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | wc -l
 
 (echo 1 | cat > alt) > out
+cat out
+cat alt
+
+(echo 1 | cat > alt) > out
+cat out
+cat alt
+
+(echo 1 | cat ) > out
+cat out
+cat alt
+
+(echo 1 | echo 2 ) > out
+cat out
+cat alt
+
+(echo 1 && echo 2 ) > out
+cat out
+cat alt

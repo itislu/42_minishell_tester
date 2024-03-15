325557177fe6853c685c984e3a92a5d46ed24d56
LeaYeh <smile2140@gmail.com>
2024-03-15 14:33:56 +0100

    feat: Add test to check subshell with redirect order

--- cmds/bonus/3_subshell.sh
+++ cmds/bonus/3_subshell.sh
@@ -8,3 +8,5 @@ yes | (echo 1 && (echo 2 | (echo 3 && echo 4))) | cat
 yes | (echo 1 && (echo 2 | (echo 3 && echo 4))) | cat | wc -l
 
 yes | (head -1 && (head -2 | (head -3 && head -4))) | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | cat | wc -l
+
+(echo 1 | cat > alt) > out

2c566cd6ebbafc1e6b33945689a25e6e256373c2
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-08-07 23:42:50 +0200

    feat: Add mstest alias to .bashrc and .zshrc files

--- install.sh
+++ install.sh
@@ -17,18 +17,25 @@ rm -rf 42_minishell_tester_tmp
 cd $HOME/42_minishell_tester
 chmod +x $HOME/42_minishell_tester/tester.sh
 
-RC_FILE=$HOME/.zshrc
+# List of rc files to update
+RC_FILES=("$HOME/.bashrc" "$HOME/.zshrc")
 
-if [[ "$SHELL" == *"bash"* ]]; then  #  works no matter path is /usr/bin/bash or /bin/bash
-    RC_FILE="$HOME/.bashrc"
-elif [[ "$SHELL" == *"zsh"* ]]; then
-    RC_FILE="$HOME/.zshrc"
-fi
+# Alias to be added
+ALIAS_LINE="\nalias mstest=\"bash $HOME/42_minishell_tester/tester.sh\"\n"
 
-if ! grep "mstest=" $RC_FILE &> /dev/null; then
-	echo "mstest alias not present"
-	echo "Adding alias in file: $RC_FILE"
-	echo -e "\nalias mstest=\"bash $HOME/42_minishell_tester/tester.sh\"\n" >> $RC_FILE
-fi
+# Function to add alias to a file if it doesn't already exist
+add_alias_if_not_present() {
+    local file=$1
+    if ! grep "mstest=" "$file" &> /dev/null; then
+        echo "mstest alias not present in $file"
+        echo "Adding alias in file: $file"
+        echo -e "$ALIAS_LINE" >> "$file"
+    fi
+}
+
+# Loop through each rc file and add the alias
+for rc_file in "${RC_FILES[@]}"; do
+    add_alias_if_not_present "$rc_file"
+done
 
 exec $SHELL
